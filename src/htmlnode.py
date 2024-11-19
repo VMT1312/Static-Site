626fdf650c502ba5818fc104cc178d9112588e79
VTM-Planfarm <vinh@planfarm.com.au>
2024-11-19 14:42:13 +0800

    Parent node for html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,12 +4,46 @@ class HTMLNode():
         self.value = value
         self.children = children
         self.props = props
+
     def to_html(self):
         raise NotImplementedError
+  
     def props_to_html(self):
         if self.props is None or self.props == {}:
             return ""
         attributes = [f'{key}={value}' for key, value in self.props.items()]
         return ' ' + ' '.join(attributes)
+
     def __repr__(self) -> str:
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+   
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None) -> None:
+        super().__init__(tag=tag, value=value, props=props, children=None)
+       
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value is mandatory")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+   
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+   
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None) -> None:
+        super().__init__(tag=tag, children=children, props=props, value=None)
+  
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag is required")
+        if self.children is None or self.children == []:
+            raise ValueError("Children(s) is required")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
-
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_no_props(self):
@@ -17,6 +16,34 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             "HTMLNode(p, text inside a paragraph, ['a', 'b'], {'href': 'https://www.google.com'})", repr(node)
         )
+
+    def test_child_no_props(self):
+        node = LeafNode("p", "This is a paragraph of text")
+        self.assertNotEqual(node.to_html(), "This is a paragraph of text")
+
+    def test_child_props(self):
+        node = LeafNode("a", "Link", {'href': 'https://www.google.com'})
+        self.assertEqual(node.to_html(), '<a href=https://www.google.com>Link</a>')
+
+    def test_child_no_tags(self):
+        node = LeafNode(value="This is a paragraph of text")
+        self.assertEqual(node.to_html(), "This is a paragraph of text")
+
+    def test_parent_no_props(self):
+        node = ParentNode("p", [LeafNode("b", "Bold text"), LeafNode(None, "Normal text")])
+        self.assertEqual("<p><b>Bold text</b>Normal text</p>", node.to_html())
+
+    def test_parent_parent(self):
+        node = ParentNode("p", 
+                          [
+            LeafNode("b", "Bold text"), 
+            ParentNode("p", [
+                LeafNode('a', 'Link', {'href': 'https://www.google.com'})
+                ]
+                )
+                ]
+                )
+        self.assertEqual("<p><b>Bold text</b><p><a href=https://www.google.com>Link</a></p></p>", node.to_html())
 
 
 if __name__ == "__main__":
