2ab35b55c54ff530a13a56c7727102e73b5e81c2
VMT1312 <thiemmavinh123@gmail.com>
2025-01-31 17:33:17 +0800

    File transfer from static to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from textnode import TextNode, TextType
+import os, shutil
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    file_transfer('static/')
     print(node)
+
+
+def file_transfer(source_path):
+    if os.path.exists('public/'):
+        shutil.rmtree('public/')
+    destination = 'public/'
+    source = source_path
+    os.mkdir(destination)
+    for file in os.listdir(source):
+        cur_path = os.path.join(source, file)
+        if os.path.isfile(cur_path):
+            shutil.copy(
+                cur_path,
+                destination
+            )
+        else:
+            file_transfer(cur_path)
+
 
 main()
